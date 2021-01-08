9eeb871bfd8f480c4d88c078018d05c25bff7084
SignatureJed <SignatureJed>
2021-01-08 17:41:06 +0700

    code completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main () {
+    double sum=0,X,S_D,N[100];
+    int k=0;
+    string str;
+    ifstream source ("score.txt");
+    while (getline(source,str)) {
+        N[k++] = stod(str);
+        sum += stod(str.c_str());
+    }
+    X = sum/k;
+    sum = 0;
+    for (int i=0;i<k;i++) sum += pow(N[i],2);
+    sum /= k;
+    S_D = sqrt(sum - pow(X,2));
+    cout << "Number of data = " << k << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << X << endl;
+    cout << "Standard deviation = " << S_D << endl;
+}
